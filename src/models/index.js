f5b9519d83a6a9a02ad0ae3e1d3f6d99846857b8
pj-able <wlawanpreda@gmail.com>
2020-09-22 00:58:25 +0700

    remove junk and set docker

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,12 @@
 import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { WebSocketLink } from "@apollo/client/link/ws";
-
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/",
+  uri: "http://46.101.1.15:4000/",
 });
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri: `ws://46.101.1.15:4000/graphql`,
   options: {
     reconnect: true,
   },
